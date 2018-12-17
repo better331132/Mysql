acd5ef4f41ee77d73cb90529332ca9e237e2c839
better331132 <better3311@naver.com>
2018-12-17 18:40:53 +0900

    cursor_team

--- procedure.sql
+++ procedure.sql
@@ -45,4 +45,32 @@ Begin
         where cm.club = v_club;
 
 end //
+delimiter ;
+call sp_club_level('요트');
+
+drop procedure sp_club_level;
+
+Delimiter //
+create procedure sp_club_level (_club_name varchar(100))
+Begin
+    declare v_club smallint;
+   select id into v_club from Club where name = _club_name;
+
+   select s.name, case_level(name)
+        from Clubmember cm  inner join student s on cm.student = s.id
+        where cm.club = v_club;
+
+end //
 Delimiter ;
+
+Delimiter //
+create procedure sp_club_members(_club_name varchar(31))
+begin
+	select s.name, (case when cm.level = 2 then '회장'
+                        when cm.level = 1 then '간부'
+                        else '평회원' end)
+      from Clubmember cm inner join Student s on cm.student = s.id
+	 where club = (select id from Club where name = _club_name)
+     order by cm.level desc;
+end //
+delimiter ;
